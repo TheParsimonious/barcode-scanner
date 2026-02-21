db2b035cc6e3897b0bbd6f94346bdbc69b19ba5b
TheParsimonious <130803978+TheParsimonious@users.noreply.github.com>
2026-02-20 23:43:22 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "barcode-scanner-v3";
+﻿const CACHE_NAME = "barcode-scanner-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -41,5 +41,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
